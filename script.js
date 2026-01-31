a75e4ea1386bb3d3eac20097aa907024c93f2a75
Mubarak <ymubarak872@gmail.com>
2026-01-31 11:45:53 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function sendMessage(e) {
+  e.preventDefault();
+  alert("Thank you! Your message has been sent.");
+}
+
+function subscribe(e) {
+  e.preventDefault();
+  alert("You have been subscribed!");
+}
